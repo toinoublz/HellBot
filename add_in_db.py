045209a43889dfdc5fe045bff803332fdd4416ee
Antoine BLAISE <antoine.blaise34@gmail.com>
2024-11-14 20:49:02 +0100

    Remove useless dbadd

--- add_in_db.py
+++ add_in_db.py
@@ -4,6 +4,3 @@ db = DB("hellbot")
 
 # Configuration du salon de logs normal
 db.add('logs_channel_id', 1306683155260117133)
-
-# Configuration du salon des super logs (pour les erreurs)
-db.add('super_logs_channel_id', 1306683155260117133)  # Remplacez par l'ID du canal souhaité
